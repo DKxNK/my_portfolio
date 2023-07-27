91629eb9cab6b8bbae7dcaea8f10eae296bc4dbc
DKxNK <dknalinga99@gmail.com>
2023-07-27 15:28:57 +0530

    minor changes 9

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -64,7 +64,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-12"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-auto pt-12"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white ">
         <div>
